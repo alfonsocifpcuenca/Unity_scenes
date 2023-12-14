c0ec1dc2f9913b98a4e756f02ce136c6f53c1341
alfonsocifpcuenca <alfonso.rodriguez@cifpcuenca.es>
2023-12-14 07:07:08 +0100

    Se implementa GameManager Singleton

--- Assets/Scripts/GameManagerSingleton.cs
+++ Assets/Scripts/GameManagerSingleton.cs
@@ -4,7 +4,15 @@ using UnityEngine;
 
 public class GameManagerSingleton : MonoBehaviour
 {
+    /*
+     * Variable privada que contendrá la instancia del Game Manager (static)
+     * */
     private static GameManagerSingleton instance;
+
+    /*
+     * Variable pública que devolverá el contenido de instacia, si existe o 
+     * creará el Game Manager -> SetupInstance
+     * */
     public static GameManagerSingleton Instance
     {
         get
@@ -17,19 +25,38 @@ public class GameManagerSingleton : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Método para inciar el Game Manager
+    /// </summary>
     private static void SetupInstance()
     {
+        /*
+         * Nos aseguramos, por precaución, que el objeto no existe, de este modo tenemos claro que unicamente
+         * va a haber un GameManagerSinglenton en nuestra escena.
+         * */
         instance = FindObjectOfType<GameManagerSingleton>();
         if (instance == null)
         {
+            /*
+             * Creamos un objeto en la escena y añadimos el propio componente GameManagerSingleton, de este modo 
+             * tenemos acceso a todas las herramientas de MonoBehaviour
+             * */
             GameObject gameObj = new GameObject();
             gameObj.name = "GameManagerSingleton";
             instance = gameObj.AddComponent<GameManagerSingleton>();
 
-
+            /*
+             * Marcamos el objeto para que no se destruya a lo largo de la aplicación
+             * */
             DontDestroyOnLoad(gameObj);
         }
     }
 
+    /*
+     * Variable pública con el contador de vidas, podremos acceder a ella desde cualquier
+     * sitio de la aplicación mediante:
+     * 
+     * GameManagerSingleton.Instace.Vidas
+     * */
     public int Vidas = 3;
 }
--- Assets/Scripts/btnRestarVida.cs
+++ Assets/Scripts/btnRestarVida.cs
@@ -1,16 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class btnRestarVida : MonoBehaviour
 {
     public void RestarVida()
     {
-        var gameManagerScript = GameObject.Find("GameManager")?.GetComponent<GameManaer>();
-
-        if (gameManagerScript != null )
-        {
-            gameManagerScript.QuitarVida();
-        }
+        GameManagerSingleton.Instance.Vidas--;
+        Debug.Log("Quedan " + GameManagerSingleton.Instance.Vidas + " vidas");
     }
 }
